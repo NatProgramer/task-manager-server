31142ccc8f4d3ce1a7236f7502b57117ed637e5d
NatProgramer <rodrigezsantiago40@gmail.com>
2023-08-29 19:23:55 -0500

    config-changes

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: ['http://localhost:5173'],
-    // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  });
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
